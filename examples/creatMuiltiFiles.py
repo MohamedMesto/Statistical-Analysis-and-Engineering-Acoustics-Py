facf609d129acd1b0da32429382d43ead289d64b
Mohamed Mesto <44340777+MohamedMesto@users.noreply.github.com>
2022-11-26 20:53:57 +0100

    Update creatMuiltiFiles.py

--- examples/creatMuiltiFiles.py
+++ examples/creatMuiltiFiles.py
@@ -3,7 +3,7 @@ import datetime
 today = datetime.date.today()
 print(today)
  
-# to creat a n number of folders in the current work directory
+# to creat a n number of Files in the current work directory
 n=int(input('the Numbers of the Folders/Files: '))
 pathname = 1
 directory = "os.getcwd()" + str(pathname)
