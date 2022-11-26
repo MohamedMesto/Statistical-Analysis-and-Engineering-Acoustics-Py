ab8af46bde031075ce782715da0dd0e993b6d3d3
Mohamed Mesto <44340777+MohamedMesto@users.noreply.github.com>
2022-11-26 20:25:08 +0100

    Update creatMuiltiFolders.py

--- examples/creatMuiltiFolders.py
+++ examples/creatMuiltiFolders.py
@@ -1,1 +1,14 @@
+import os
+import datetime
+today = datetime.date.today()
+print(today)
+# to creat a n number of folders in the current work directory
+n=int(input('the Numbers of the Folders/Files'))
+pathname = 1
+directory = "os.getcwd()" + str(pathname)
 
+while pathname < n:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    pathname += 1  
+    directory = "os.getcwd()" + str(pathname)
