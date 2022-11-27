55a87dc98a1eaf7a75b55826575417a94e18d94a
Mohamed Mesto <44340777+MohamedMesto@users.noreply.github.com>
2022-11-27 12:39:45 +0100

    Update OpenBrowser-LinkTab.py

--- examples/OpenBrowser-LinkTab.py
+++ examples/OpenBrowser-LinkTab.py
@@ -1,1 +1,38 @@
+#open a Browser - Link and a Tab multiple times 
+import os
+import datetime
+today = datetime.date.today()
+print(today)
 
+n=int(input('insert hte number of the iteration: '))
+T=0
+
+
+from selenium import webdriver
+# Define the URL's we will open and a few other variables 
+main_url = 'https://github.com/MohamedMesto' # URL A
+tab_url = 'https://github.com/MohamedMesto/AWT-PJ-ss22-Video-Streaming-Mixer-Library-1' # URL B
+chromedriver = 'DESCTINATION_TO_YOUR_CHROME_DRIVER'
+
+# Open main window with URL A
+browser= webdriver.Chrome(chromedriver)
+
+
+
+for i in range(n):
+    T=T+1
+    if T>30 :
+        # Open main window with URL A
+        browser= webdriver.Chrome(chromedriver)
+        T=0
+
+
+    browser.get(main_url)
+    print("Current Page Title is : %s" %browser.title)
+    # Open a new window
+    browser.execute_script("window.open('');")
+    #Open a new window
+    browser.execute_script("window.open('');")
+    # Switch to the new window and open URL B
+    browser.switch_to.window(browser.window_handles[1])
+    browser.get(tab_url)
