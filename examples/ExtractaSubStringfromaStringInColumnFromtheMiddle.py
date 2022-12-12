1ef686040736cb105c179984d525ce4391902d0a
Mohamed Mesto <44340777+MohamedMesto@users.noreply.github.com>
2022-12-12 22:53:02 +0100

    Update ExtractaSubStringfromaStringInColumnFromtheMiddle.py

--- examples/ExtractaSubStringfromaStringInColumnFromtheMiddle.py
+++ examples/ExtractaSubStringfromaStringInColumnFromtheMiddle.py
@@ -1,1 +1,14 @@
 
+# Duration distribution of audiofiles per speaker's accent AT,CA, CH, DE_AL, DE_ni, DE, FR, GB, IT, RU, US. both as a plot and in terms of mean and variance.
+import re # import the library of research about special value in String 
+################################## Accent AT ###########################################
+print('################################## Accent AT ###########################################')
+# add Columns Names to the Dataset: header=None, names=['audio_filepath','text','duration']
+dataset_test_at = pd.read_csv('test_at.txt', header=None, names=['audio_filepath','text','duration'])
+
+
+###########################################################################################
+### How To Extract a SubString from a String In Column From the Middle ==> .str[-12:-4]
+dataset_test_at['audio_filepath(numeric)'] = dataset_test_at['audio_filepath'].str.slice(0, 7)
+
+print(dataset_test_at)
