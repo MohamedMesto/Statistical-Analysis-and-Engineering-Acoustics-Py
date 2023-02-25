fecb5c76b3657678e68111ae406caf1431d4cc61
Mohamed Mesto <44340777+MohamedMesto@users.noreply.github.com>
2023-02-25 20:34:29 +0100

    Create displyIndexOfaValueFromTsvFile.py

---
+++ examples/displyIndexOfaValueFromTsvFile.py
@@ -0,0 +1,61 @@
+# disply Index Of a Value From a Tsv File
+
+! cp /content/drive/MyDrive/QU-DFKI-Thesis-ASR/Experimentation/cv-corpus04072022/de/validated.tsv /content/validated.tsv
+
+
+'''
+client_id	path	sentence	up_votes	down_votes	age	gender	accents	locale	segment
+674b147a9ae0e407343da77a6f7d83bbf481bacc12f5d0427dfddfda5dfbc4c8a769f4d6c626a7ec53892825d02a0fcc8289c92ce677df3c5fed2b978206f604	common_voice_de_32440429.mp3	Das Schutzgebiet gehört zum Naturraum Südwestliches Albvorland.	2	0				de	
+690943450b5e554ad844d9896ff23fab52520ef7812e91747fabf6a97b610969ffcc64af40b9b2470868dcda71ef438d6243cbda627cc1625f2ad0fdbac70f16	common_voice_de_32450224.mp3	Die Handlung dreht sich um den Alltag der benachbarten Familien Gibbs und Webb.	2	0				de	
+690943450b5e554ad844d9896ff23fab52520ef7812e91747fabf6a97b610969ffcc64af40b9b2470868dcda71ef438d6243cbda627cc1625f2ad0fdbac70f16	common_voice_de_32450225.mp3	Afroamerikaner benutzten es in jener Zeit erstmals in Alabama.	2	0				de	
+734ec7ed13859ed05c5c05b04d8cf349367c81b6505702e66d0e8551507197ebc09430641878fedfdc7802124eefb0d85945ddeee0795cf3199f36b8bffb679d	common_voice_de_31857874.mp3	Die Notwendigkeit einer Reform der Polizei in Indien ist seit langem anerkannt.	2	0	teens	male		de	
+734ec7ed13859ed05c5c05b04d8cf349367c81b6505702e66d0e8551507197ebc09430641878fedfdc7802124eefb0d85945ddeee0795cf3199f36b8bffb679d	common_voice_de_31857875.mp3	Die Auflistung ist in zwei Teile gegliedert.	2	0	teens	male		de	
+7cc2bdc7ce6f56a87fe0d4401fe26d604885d00dd0974324f5c6158ce84fb7fab8e44f44d352081933fed127a8fb2a0db73682eeda7899dba4a81706ca9053ca	common_voice_de_32786446.mp3	Die Bebauung geht im Osten direkt in das Stadtgebiet von Bad Lausick über.	2	0				de	
+7cc2bdc7ce6f56a87fe0d4401fe26d604885d00dd0974324f5c6158ce84fb7fab8e44f44d352081933fed127a8fb2a0db73682eeda7899dba4a81706ca9053ca	common_voice_de_32786453.mp3	Harry lässt sie jedoch links liegen und fährt fort.	3	0				de	
+839d69565649835cfdc9cae3f0eaf337d29896a658279f897e74d403d5868c0d839476ec1a7bd49bdd3cc512e30d58362b2c12f13bb461c8c8d7ecd6feb9b0db	common_voice_de_32440723.mp3	Außerdem fanden sich auch Partikel von purem Gold.	2	0				de	
+839d69565649835cfdc9cae3f0eaf337d29896a658279f897e74d403d5868c0d839476ec1a7bd49bdd3cc512e30d58362b2c12f13bb461c8c8d7ecd6feb9b0db	common_voice_de_32440724.mp3	Bei der kurz darauf folgenden Wahl wurde Brown unter anderem von Bill Clinton unterstützt.	2	0				de	
+88cf816ef1757d883fa755d6a45044ed23618b370befcbe1e98eab412d7f26da1a0d5954253b7c077123807397dc5636021f82298de5c4392c7e56a1a35a325f	common_voice_de_32344759.mp3	Jedoch unterlag sie mit ihrem Team im Finale gegen Australien.	4	0	thirties	male	Deutschland Deutsch	de	
+88cf816ef1757d883fa755d6a45044ed23618b370befcbe1e98eab412d7f26da1a0d5954253b7c077123807397dc5636021f82298de5c4392c7e56a1a35a325f	common_voice_de_32344769.mp3	Die anderen drei wurden mit den Teams der Qualifikationsrunden gebildet.	4	0	thirties	male	Deutschland Deutsch	de	
+94abe340330ef8d28641a9f5df34959fd042802a6cf5fe6846af0e7f2a783184bf88e5e5a189c7d4d8c0f9d125d388c520eefdbf19b937ffa94f97a4d5bc6d9f	common_voice_de_32166130.mp3	Der Junge Barry Christopher setzt versehentlich sein Haus in Brand.	2	0				de	
+94abe340330ef8d28641a9f5df34959fd042802a6cf5fe6846af0e7f2a783184bf88e5e5a189c7d4d8c0f9d125d388c520eefdbf19b937ffa94f97a4d5bc6d9f	common_voice_de_32166131.mp3	Wie der Elch bis Frankfurt kommt.	2	0				de	
+bee1edd1894bb36d89bb422a69d076f4659b9c999049cfc29d146fef173d1d13c4c62c6e5266de51261524b5c1c995a90c1d5a8644caf09cdea5333d1790080f	common_voice_de_32383391.mp3	Bei Shenzhen in Guangdong gelangte das System über Land.	2	1				de	
+bee1edd1894bb36d89bb422a69d076f4659b9c999049cfc29d146fef173d1d13c4c62c6e5266de51261524b5c1c995a90c1d5a8644caf09cdea5333d1790080f	common_voice_de_32383392.mp3	Mowbray ist Teil der Metropolgemeinde "City of Cape Town".	2	1				de	
+dee0143aef5d93333884a9a3471f9ec28585579e20a22e714cfc492cfe20f7c426657a227757c62dd45825f1e2a05201ab1fea06e6f1005ab1284ec1fdd2291f	common_voice_de_32697548.mp3	Dies wird als bezeichnet.	2	1				de	
+dee0143aef5d93333884a9a3471f9ec28585579e20a22e714cfc492cfe20f7c426657a227757c62dd45825f1e2a05201ab1fea06e6f1005ab1284ec1fdd2291f	common_voice_de_32697550.mp3	Dieser sei in der Woche vor dem Angriff erschossen worden.	2	0				de	
+f70fe05b4895840230c0f5ece4830ce926d1df542bec8b33029364b57e6273d9a29c2bfa5a1f6117f90bbaa73300deeb11109217a588eebe297f177f16d02e28	common_voice_de_32330683.mp3	Er war zudem auch schwedischer Juniorennationalspieler.	2	0	fifties	male	Österreichisches Deutsch	de	
+f70fe05b4895840230c0f5ece4830ce926d1df542bec8b33029364b57e6273d9a29c2bfa5a1f6117f90bbaa73300deeb11109217a588eebe297f177f16d02e28	common_voice_de_32330689.mp3	Während seines Studiums legte Möller an der britischen Newcastle University einen Auslandsaufenthalt ein.	2	0	fifties	male	Österreichisches Deutsch	de	
+0f6857b3eddd4709752d6dfb7332bf0ed2985960733924742682937c97102d4fc7214c81a6082c43f7b2f47ac7a0386ebefe5dfd50d5b316a4988c2c9d995ab2	common_voice_de_32300404.mp3	Auf Ersuchen des Premierministers bleiben sie im Amt.	2	0				de	
+0f6857b3eddd4709752d6dfb7332bf0ed2985960733924742682937c97102d4fc7214c81a6082c43f7b2f47ac7a0386ebefe5dfd50d5b316a4988c2c9d995ab2	common_voice_de_32300405.mp3	In Silber schrägrechts liegender roter Maueranker.	2	0				de	
+
+'''
+
+
+
+
+
+import pandas as pd
+tsv_data = pd.read_csv('validated.tsv', sep='\t')
+tsv_data.drop(tsv_data[(tsv_data['accents'].isna())].index, inplace=True)
+tsv_data
+print('*******************print Labels************************')
+
+for label, content in tsv_data.items():
+    print(f'label: {label}')
+    #print(f'content: {content}', sep='\n')
+
+print('*********************print Contents**********************')
+
+for label, content in tsv_data.items():
+    #print(f'label: {label}')
+    print(f'content: {content}', sep='\n')
+    
+##########################################################################
+
+# for the Value common_voice_de_19756404.mp3 index_var has the index of the row
+index_var = tsv_data[tsv_data['path'] == 'common_voice_de_32344769.mp3'].index[0]
+print(index_var)
+# will print 11
+
+tsv_data['accents'][index_var]
+# will print Deutschland Deutsch	
+
